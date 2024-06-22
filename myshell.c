62e0345cfb51ad1cc7fa8fc5e3f905772bc823c3
GuyKoch <guykoch@mail.tau.ac.il>
2024-06-22 13:16:58 +0300

    Feature: basic command execution works

--- myshell.c
+++ myshell.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 /*
 This function prepares the shell for command execution
@@ -8,6 +12,41 @@ Returns:
 */
 int prepare(void){
     return 0;
+}
+
+
+/*
+
+This function executes shell command, meaning case 1 of section 1.1
+
+Params: 
+    arglist: parsed command to array
+Returns:
+    the result of the execution: 
+        1 for successful
+        0 for unsuccessful
+
+*/
+int execute_command(char **arglist){
+    pid_t pid;
+    pid = fork();
+    if(pid == 0){
+        execvp(arglist[0], arglist);
+		printf("execvp error: %s\n", strerror(errno));
+		return 0;    
+    }
+    else if (pid > 0){
+        int result;
+        waitpid(pid, &result, 0);
+        if(!WIFEXITED(result)){
+            return 0;
+        }
+        return 1;
+    }
+    else{
+        printf("fork error: %s\n", strerror(errno));
+        return 0;
+    }
 }
 
 /*
@@ -23,6 +62,30 @@ Returns:
 
 */
 int process_arglist(int count, char **arglist){
+    
+    int i, command_flag, background_flag, pipe_flag, input_flag, output_flag = 0;
+    for(i=0; i < count; i++){
+        if(strcmp(arglist[i],"&") == 0){
+            background_flag = 1;
+        }
+        else if (strcmp(arglist[i],"|") == 0){
+            pipe_flag = 1;
+        }
+        else if (strcmp(arglist[i],"<") == 0){
+            input_flag = 1;
+        }
+        else if (strcmp(arglist[i],">>") == 0){
+            output_flag = 1;
+        }
+    }
+    if(!(background_flag && pipe_flag && input_flag && output_flag)){
+        command_flag = 1;
+    }
+    
+    if(command_flag){
+        return execute_command(arglist);
+    }
+    
     return 1;
 }
 
